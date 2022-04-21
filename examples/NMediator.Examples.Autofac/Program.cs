72743d21a81cd7086673a9f9403e2db3cc3ea6b9
Mars.P <marsxxd@outlook.com>
2022-04-21 08:31:18 +0800

    add autofac example

---
+++ examples/NMediator.Examples.Autofac/Program.cs
@@ -0,0 +1,49 @@
+﻿using Autofac;
+using NMediator.Examples.Base;
+using NMediator.Examples.Filters.CommandFilters;
+using NMediator.Examples.Filters.EventFilters;
+using NMediator.Examples.Filters.ExceptionFilters;
+using NMediator.Examples.Filters.MessageFilters;
+using NMediator.Examples.Filters.RequestFilters;
+using NMediator.Examples.Messages.Commands;
+using NMediator.Examples.Middlewares;
+using NMediator.Examples.Services;
+using NMediator.Extensions.Autofac;
+
+namespace NMediator.Examples.Autofac;
+
+public static class Program
+{
+    public static Task Main(string[] args)
+    {
+        var builder = new ContainerBuilder();
+
+        var logger = new Logger();
+        
+        builder.RegisterInstance(logger).SingleInstance();
+        builder.RegisterType<LogService>().As<ILogService>().InstancePerLifetimeScope();
+        builder.RegisterType<DoNothingService>().As<IDoNothingService>().InstancePerLifetimeScope();
+
+        builder.RegisterNMediator(config =>
+        {
+            config
+                .UseMiddleware<ExampleMiddleware1>()
+                .UseMiddleware<ExampleMiddleware2>()
+                .UseFilter<AllMessagesFilter>()
+                .UseFilter<ExampleCommandMessageFilter>()
+                .UseFilter<AllCommandsFilter>()
+                .UseFilter<ExampleCommandFilter>()
+                .UseFilter<AllEventsFilter>()
+                .UseFilter<ExampleEventFilter>()
+                .UseFilter<AllRequestsFilter>()
+                .UseFilter<ExampleRequestFilter>()
+                .UseFilter<ExceptionFilter>();
+        }, typeof(ExampleCommand).Assembly);
+        
+        var container = builder.Build();
+
+        var mediator = container.Resolve<IMediator>();
+        
+        return Runner.Run(mediator, logger, "Autofac");
+    }
+}
